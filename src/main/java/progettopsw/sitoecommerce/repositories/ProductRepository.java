eb1f406af7cc04a295ec9eaeeef35661310720bc
Salvatore Anile <salvatoreanile629@gmail.com>
2022-05-01 17:47:18 +0200

    Modified ProductRepository

--- src/main/java/progettopsw/sitoecommerce/repositories/ProductRepository.java
+++ src/main/java/progettopsw/sitoecommerce/repositories/ProductRepository.java
@@ -1,6 +1,7 @@
 package progettopsw.sitoecommerce.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import progettopsw.sitoecommerce.entities.Product;
 
@@ -12,6 +13,9 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
     List<Product> findByNameContaining(String name); //trova per nome che contiene la stringa passata come parametro
     List<Product> findByBarCode(String name);
     boolean existsByBarCode(String barCode);
+
+    @Query("select p from Product p where (p.name like ?1 or ?1 is null) and (p.quantity > ?2 or ?2 is null) and (p.price > ?3 or ?3 is null)")
+    List<Product> advancedSearch(String name, int quantity, int price);
 
 
 }//ProductRepository
