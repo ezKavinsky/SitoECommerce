35caa4e1431de2e2a5b6380317a66fceda287a81
Salvatore Anile <salvatoreanile629@gmail.com>
2022-05-11 11:18:34 +0200

    Modified AccountingService

--- src/main/java/progettopsw/sitoecommerce/services/AccountingService.java
+++ src/main/java/progettopsw/sitoecommerce/services/AccountingService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import progettopsw.sitoecommerce.entities.User;
 import progettopsw.sitoecommerce.repositories.UserRepository;
-
 import javax.transaction.Transactional;
 import java.util.List;
 
