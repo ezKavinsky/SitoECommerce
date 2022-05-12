e69c7b276d80162e0a5f055970bc9d6c3b3a8d68
Salvatore Anile <salvatoreanile629@gmail.com>
2022-05-12 12:33:20 +0200

    Added Accounting Controller

--- src/main/java/progettopsw/sitoecommerce/controllers/AccountingController.java
+++ src/main/java/progettopsw/sitoecommerce/controllers/AccountingController.java
@@ -8,6 +8,7 @@ import progettopsw.sitoecommerce.entities.User;
 import progettopsw.sitoecommerce.exceptions.MailUserAlreadyExistsException;
 import progettopsw.sitoecommerce.services.AccountingService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -18,7 +19,7 @@ public class AccountingController {
     private AccountingService accountingService;
 
     @PostMapping
-    public ResponseEntity create(@RequestBody User user){
+    public ResponseEntity create(@RequestBody @Valid User user){
         try{
             User added = accountingService.registerUser(user);
             return new ResponseEntity(added, HttpStatus.OK);
